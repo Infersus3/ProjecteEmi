3383582dfb71d137451ae6e10715af720de97f2f
Sale <saleamlak_secorunmontiel@iescarlesvallbona.cat>
2021-04-06 21:19:09 +0200

    migracions login

---
+++ database/migrations/2021_04_06_181650_create_professors_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfessorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('professors', function (Blueprint $table) {
+            $table->id();
+            $table->string('dni');
+            $table->string('nom');
+            $table->string('cognom');
+            $table->date('dataN');
+            $table->foreignId('user_id')->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('professors');
+    }
+}
