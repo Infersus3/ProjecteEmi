708165173ee68a419e3969d2991754ddde11b2ca
Sale <saleamlak_secorunmontiel@iescarlesvallbona.cat>
2021-04-09 20:48:41 +0200

    començament vista admin

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="wrapper">
     <div class="row justify-content-center">
         <div class="col-md-4">
             <div class="card">
@@ -10,35 +10,138 @@
                 </div>
                 <div class="card-body">
                     <div class="card-body">
-                        <p>yeees</p>
+                    <table class="table table-responsive">
+                <tr>
+                <td>
+                    <h5 class="users">Joan Sànchez </h5>
+                </td>
+                <td>
+                    <button id="btnGroupDrop1" type="button" class="btn btn-sm btn-secondary dropdown-toggle" data-toggle="dropdown"> Assignar </button>
+                    <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+                        <a class="dropdown-item" href="#!">Usuari</a>
+                        <a class="dropdown-item" href="#!">Alumne</a>
+                        <a class="dropdown-item" href="#!">Professor</a>
+                        <a class="dropdown-item" href="#!">Admin</a>
+                    </div>
+                </td>
+                <td>
+                    <button id="btnGroupDrop1" type="button" class="btn btn-sm btn-danger btn-secondary" > Eliminar </button>
+                </td>
+                </tr>
+                    <tr>
+                <td>
+                    <h5 class="users">Pau Peres </h5>
+                    </td>
+                    <td>
+                    <button id="btnGroupDrop1" type="button" class="btn btn-sm btn-secondary dropdown-toggle" data-toggle="dropdown"> Assignar </button>
+                    <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+                        <a class="dropdown-item" href="#!">Usuari</a>
+                        <a class="dropdown-item" href="#!">Alumne</a>
+                        <a class="dropdown-item" href="#!">Professor</a>
+                        <a class="dropdown-item" href="#!">Admin</a>
+                    </div>
+                    </td>
+                    <td>
+                    <button id="btnGroupDrop1" type="button" class="btn btn-sm btn-danger btn-secondary" > Eliminar </button>
+                </td>
+                    </tr>
+                </table>
                     </div>
                 </div>
             </div>
         </div>
         <div class="col-md-4">
-            <div class="card">
+        <div class="card">
                 <div class="card-header">
-                    <h5 class="card-title">Alumnes</h5>
-                    
+                    <h5 class="card-title">Professors</h5>
                 </div>
                 <div class="card-body">
-                    <p>prova sale</p>
-                    
+                    <div class="card-body">
+                    <table class="table table-responsive">
+                <tr>
+                <td>
+                    <h5 class="users">Joan Sànchez </h5>
+                </td>
+                <td>
+                    <button id="btnGroupDrop1" type="button" class="btn btn-sm btn-secondary dropdown-toggle" data-toggle="dropdown"> Assignar </button>
+                    <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+                        <a class="dropdown-item" href="#!">Usuari</a>
+                        <a class="dropdown-item" href="#!">Alumne</a>
+                        <a class="dropdown-item" href="#!">Professor</a>
+                        <a class="dropdown-item" href="#!">Admin</a>
+                    </div>
+                </td>
+                <td>
+                    <button id="btnGroupDrop1" type="button" class="btn btn-sm btn-danger btn-secondary" > Eliminar </button>
+                </td>
+                </tr>
+                    <tr>
+                <td>
+                    <h5 class="users">Pau Peres </h5>
+                    </td>
+                    <td>
+                    <button id="btnGroupDrop1" type="button" class="btn btn-sm btn-secondary dropdown-toggle" data-toggle="dropdown"> Assignar </button>
+                    <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+                        <a class="dropdown-item" href="#!">Usuari</a>
+                        <a class="dropdown-item" href="#!">Alumne</a>
+                        <a class="dropdown-item" href="#!">Professor</a>
+                        <a class="dropdown-item" href="#!">Admin</a>
+                    </div>
+                    </td>
+                    <td>
+                    <button id="btnGroupDrop1" type="button" class="btn btn-sm btn-danger btn-secondary" > Eliminar </button>
+                </td>
+                    </tr>
+                </table>
+                    </div>
                 </div>
-
             </div>
         </div>
         <div class="col-md-4">
-            <div class="card">
+        <div class="card">
                 <div class="card-header">
-                    <h5 class="card-title">Usuaris</h5>
-                    
+                    <h5 class="card-title">Professors</h5>
                 </div>
                 <div class="card-body">
-                    <p>yeees</p>
-                    
+                    <div class="card-body">
+                    <table class="table table-responsive">
+                <tr>
+                <td>
+                    <h5 class="users">Joan Sànchez </h5>
+                </td>
+                <td>
+                    <button id="btnGroupDrop1" type="button" class="btn btn-sm btn-secondary dropdown-toggle" data-toggle="dropdown"> Assignar </button>
+                    <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+                        <a class="dropdown-item" href="#!">Usuari</a>
+                        <a class="dropdown-item" href="#!">Alumne</a>
+                        <a class="dropdown-item" href="#!">Professor</a>
+                        <a class="dropdown-item" href="#!">Admin</a>
+                    </div>
+                </td>
+                <td>
+                    <button id="btnGroupDrop1" type="button" class="btn btn-sm btn-danger btn-secondary" > Eliminar </button>
+                </td>
+                </tr>
+                    <tr>
+                <td>
+                    <h5 class="users">Pau Peres </h5>
+                    </td>
+                    <td>
+                    <button id="btnGroupDrop1" type="button" class="btn btn-sm btn-secondary dropdown-toggle" data-toggle="dropdown"> Assignar </button>
+                    <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+                        <a class="dropdown-item" href="#!">Usuari</a>
+                        <a class="dropdown-item" href="#!">Alumne</a>
+                        <a class="dropdown-item" href="#!">Professor</a>
+                        <a class="dropdown-item" href="#!">Admin</a>
+                    </div>
+                    </td>
+                    <td>
+                    <button id="btnGroupDrop1" type="button" class="btn btn-sm btn-danger btn-secondary" > Eliminar </button>
+                </td>
+                    </tr>
+                </table>
+                    </div>
                 </div>
-
             </div>
         </div>
     </div>
